82da26154469dd330eddac99da55802294182733
Vyacheslav Shershnev <ermak519@gmail.com>
2022-04-11 21:39:57 +0300

    исправление замечаний по старому коду, доработка формы регистрации и кнопок логина через соцсети

--- pages/user/RegOrLoginSocial.tsx
+++ pages/user/RegOrLoginSocial.tsx
@@ -1,15 +1,20 @@
-import { ContinueWithSocial, SocialLink, SocialImg } from "./userLoginOrRegisterStyle"
-import apple from '../../common/images/icons/apple.png'
-import Facebook from '../../common/images/icons/Facebook.png';
-import google from '../../common/images/icons/google.png';
+import { ContinueWithSocial, SocialLink } from './userLoginOrRegisterStyle';
 
-export const RegOrLoginSocial = () => (
-	<ContinueWithSocial>
-		<div>or continue with</div>
-		<SocialLink>
-			<a href='#' ><SocialImg width='36px' src={apple.src}></SocialImg></a>
-			<a href='#' ><SocialImg width='36px' src={Facebook.src}></SocialImg></a>
-			<a href='#' ><SocialImg width='36px' src={google.src}></SocialImg></a>
-		</SocialLink>
-	</ContinueWithSocial>
-)
+import GoogleBtn from './SocialsBtns/GoogleBtn';
+import MailBtn from './SocialsBtns/MailBtn';
+import VkBtn from './SocialsBtns/VkBtn';
+import YandexBtn from './SocialsBtns/YandexBtn';
+
+export const RegOrLoginSocial = () => {
+  return (
+    <ContinueWithSocial>
+      <div>or continue with</div>
+      <SocialLink>
+        <GoogleBtn />
+        <MailBtn />
+        <VkBtn />
+        <YandexBtn />
+      </SocialLink>
+    </ContinueWithSocial>
+  );
+};
