d8da840d8cd4d6b369c3ba821b47cc6b5793c7df
Vyacheslav Shershnev <ermak519@gmail.com>
2022-04-12 23:40:28 +0300

    добавил модели для полей и начальных значений в форме регистрации

--- pages/user/register/RegisterFormStep.tsx
+++ pages/user/register/RegisterFormStep.tsx
@@ -2,113 +2,40 @@ import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { CardContent } from '@mui/material';
 
-import {  IRegisterForm, IFormStatus  } from '../../../model/loginOrRegisterInterfaces/interfaces';
+import { IRegisterForm, IFormStatus } from '../../../model/loginOrRegisterInterfaces/interfaces';
+import {
+  formStatusValue,
+  formRegisterValues,
+} from '../../../model/loginOrRegisterInterfaces/initialValues';
+import {
+  userInfo,
+  exercises,
+  mealPreferenciesSelect,
+} from '../../../model/loginOrRegisterInterfaces/inputsValues';
 
 import { FormTextField } from '../../../common/user/FormTextField';
 import { FormSelectField } from '../../../common/user/FormSelectField';
 import FormikStep from '../../../common/user/FormikStep';
 
-import { validationUser, validationMeal, validationExercises} from '../../../utils/validationSchema';
+import {
+  validationUser,
+  validationMeal,
+  validationExercises,
+} from '../../../utils/validationSchema';
 import { loginOrRegisterUser } from '../../../utils/loginOrRegisterUser';
 import { redirectToLoginPage } from '../../../utils/redirect';
 import { paused } from '../../../utils/paused';
-import {LOGIN_PAGE} from '../../../utils/urls';
+import { LOGIN_PAGE } from '../../../utils/urls';
 
 import { RightSide } from '../userLoginOrRegisterStyle';
 import { FormikStepper } from './FormikStepper';
 
-
 export const RegisterForm: React.FC = () => {
   const [displayFormStatus, setDisplayFormStatus] = useState<boolean>(false);
 
-  const [formStatus, setFormStatus] = useState<IFormStatus>({
-    message: '',
-    type: '',
-  });
+  const [formStatus, setFormStatus] = useState<IFormStatus>(formStatusValue);
 
-  const [values, setValues] = useState<IRegisterForm>({
-    email: '',
-    password: '',
-    confirmPassword: '',
-    userName: '',
-    contactNumber: null,
-    prohibitedProducts: '',
-    wishProducts: '',
-    goal: '',
-    quantityMeals: '',
-    typeOfProgramm: '',
-    numberOfWorkouts: '',
-    typeOfTrainings: '',
-  });
-
-  const userInfo = [
-    {
-      placeholder: 'Enter Email',
-      name: 'email',
-      type: 'text',
-      secrecy: false
-    },
-    {
-      placeholder: 'Create User name',
-      name: 'userName',
-      type: 'text',
-      secrecy: false
-    },
-    {
-      placeholder: 'Contact number',
-      name: 'contactNumber',
-      type: 'text',
-      secrecy: false
-    },
-    {
-      placeholder: 'Password',
-      name: 'password',
-      type: 'password',
-      secrecy: true
-    },
-    {
-      placeholder: 'Confirm Password',
-      name: 'confirmPassword',
-      type: 'password',
-      secrecy: true
-    }
-  ];
-
-  const exercises = [
-    {
-      placeholder: 'Choose type of programm',
-      name: 'typeOfProgramm',
-      values: [
-        { value: 'aaa', title: 'aaa' },
-        { value: 'bbb', title: 'bbb' },
-        { value: 'ccc', title: 'ccc' },
-      ]
-    },
-    {
-      placeholder: 'Number of workouts per week',
-      name: 'numberOfWorkouts',
-      values: [
-        { value: 'ddd', title: 'ddd' },
-        { value: 'eee', title: 'eee' },
-        { value: 'fff', title: 'fff' },
-      ]
-    },
-    {
-      placeholder: '',
-      name: 'typeOfTrainings',
-      values: [
-        { value: 'ggg', title: 'ggg' },
-        { value: 'hhh', title: 'hhh' },
-        { value: 'iii', title: 'iii' },
-      ]
-    },
-  ];
-
-  const mealPreferenciesSelect = [
-    { value: 'jjj', title: 'jjj' },
-    { value: 'kkk', title: 'kkk' },
-    { value: 'lll', title: 'lll' },
-  ]
+  const [values, setValues] = useState<IRegisterForm>(formRegisterValues);
 
   return (
     <RightSide>
@@ -123,26 +50,45 @@ export const RegisterForm: React.FC = () => {
           }}
           initialValues={values}>
           <FormikStep label="userInfo" validationSchema={validationUser}>
-            {
-              userInfo.map((field) => {
-                return <FormTextField key={nanoid()} placeholder={field.placeholder} name={field.name} type={field.type} secrecy={field.secrecy} />
-              })
-            }
+            {userInfo.map((field) => {
+              return (
+                <FormTextField
+                  key={nanoid()}
+                  placeholder={field.placeholder}
+                  name={field.name}
+                  type={field.type}
+                  secrecy={field.secrecy}
+                />
+              );
+            })}
           </FormikStep>
 
           <FormikStep label="mealPreferencies" validationSchema={validationMeal}>
-            <FormTextField placeholder="Enter prohibited products" name="prohibitedProducts" type="text"/>
+            <FormTextField
+              placeholder="Enter prohibited products"
+              name="prohibitedProducts"
+              type="text"
+            />
             <FormTextField placeholder="Enter Wish products" name="wishProducts" type="text" />
-            <FormSelectField placeholder="Choose Goal" name="goal" values={mealPreferenciesSelect} />
+            <FormSelectField
+              placeholder="Choose Goal"
+              name="goal"
+              values={mealPreferenciesSelect}
+            />
             <FormTextField placeholder="Quantity meals a day" name="quantityMeals" type="text" />
           </FormikStep>
 
           <FormikStep label="exercises" validationSchema={validationExercises}>
-            {
-              exercises.map((field)=>{
-                return <FormSelectField key={nanoid()} placeholder={field.placeholder} name={field.name} values={field.values}/>
-              })
-            }
+            {exercises.map((field) => {
+              return (
+                <FormSelectField
+                  key={nanoid()}
+                  placeholder={field.placeholder}
+                  name={field.name}
+                  values={field.values}
+                />
+              );
+            })}
           </FormikStep>
         </FormikStepper>
       </CardContent>
