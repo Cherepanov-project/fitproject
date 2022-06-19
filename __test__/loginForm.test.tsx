ea3bf4fc99a3211f1bb8df95112e701b613acb11
Nikolay Shutikov <nikolayshutikov@gmail.com>
2022-06-19 22:34:09 +0700

    test with warning

--- __test__/loginForm.test.tsx
+++ __test__/loginForm.test.tsx
@@ -1,43 +1,37 @@
 /**
  * @jest-environment jsdom
  */
-import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-import { LoginForm } from '../pages/user/login/loginForm';
-import { nanoid } from 'nanoid';
-
-jest.mock("nanoid", () => {
-  return { nanoid: () => "1234" };
-});
-
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it("Required fields", () => {
-  const onSubmit = jest.fn();
-  act(() => {
-    render(<LoginForm/>, container);
-  });
-
-  // get a hold of the button element, and trigger some clicks on it
-  const buttons = document.querySelectorAll("button");
-  // const error_messages = document.querySelectorAll("form div div:nth-child(2)");
-  // const error_messages = document.querySelectorAll("form div div:nth-child(1)");
-  // act(() => {
-  //   buttons[1].dispatchEvent(new MouseEvent("click", { bubbles: true }));
-  // });
-  expect(buttons[1].innerHTML).toBe('Login')
-  // expect(document.querySelector("form div div:nth-child(2)").innerHTML).toBe('Required');  
-});
+ import React from "react";
+ import { render, unmountComponentAtNode } from "react-dom";
+ import { act } from "react-dom/test-utils";
+ import { LoginForm } from '../pages/user/login/loginForm';
+ import { nanoid } from 'nanoid';
+ 
+ jest.mock("nanoid", () => {
+   return { nanoid: () => "1234" };
+ });
+ 
+ let container = null;
+ beforeEach(() => {
+   // setup a DOM element as a render target
+   container = document.createElement("div");
+   document.body.appendChild(container);
+ });
+ 
+ afterEach(() => {
+   // cleanup on exiting
+   unmountComponentAtNode(container);
+   container.remove();
+   container = null;
+ });
+ 
+ it("Required fields", async () => {
+   act(() => {
+     render(<LoginForm/>, container);
+   });
+   const buttons = document.querySelectorAll("button");
+   act(() => {
+     buttons[1].dispatchEvent(new MouseEvent("click", { bubbles: true }));
+   }); 
+   setTimeout(()=>{expect(document.querySelector("form div div:nth-child(2)").innerHTML).toBe('Required')},1000);
+ });
