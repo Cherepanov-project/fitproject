d25afb5ffd605160362ddbd464cd64d453890df5
timikos <timikos@mail.ru>
2022-06-23 17:02:45 +0300

    minor

--- API/workouts.ts
+++ API/workouts.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001/',
+    baseURL: 'https://spring-boot-fitness-backend.herokuapp.com/',
     responseType: 'json',
     headers: {
-
+        Authorization: "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0aW1pa29zIiwiZXhwIjoxNjU2MDA3Nzg2LCJpYXQiOjE2NTU5ODk3ODZ9.cKU2MUzrl62lGI_OlnYWgvEFMPQPMhXRSJ1tmjfiGC0WJNmjoVhZFB41v6wcmNEEsLACnCpNDh3Au0seDx4CCQ",
     }
 })
 
 export const fetchWorkouts = async () => {
-    const response = await instance.get('workouts')
+    const response = await instance.get('api/v1/admin/exercise')
     return response
 }
 
