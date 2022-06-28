6ac571dce2d95a72cae23c813bc3066adfa3e2a8
timikos <timikosbison@gmail.com>
2022-06-28 19:17:58 +0300

    add API

--- common/workoutList/itemList/ItemList.tsx
+++ common/workoutList/itemList/ItemList.tsx
@@ -1,68 +1,78 @@
-import Link from 'next/link';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-import {useState} from "react";
-import useMediaQuery  from '@mui/material/useMediaQuery';
-
-import {exerciseList, muscleCheckboxListType} from '../../../model/workout/workout';
-import img from '../../images/workoutExercise.svg'
+import Link from "next/link"
+import Box from "@mui/material/Box"
+import Card from "@mui/material/Card"
+import Pagination from "@mui/material/Pagination"
+import Stack from "@mui/material/Stack"
+import { useState } from "react"
+import useMediaQuery from "@mui/material/useMediaQuery"
+import { useQuery } from "react-query"
 
 import { ImgWrapper, TextWrapper, Exercise, Reps } from "./ItemListStyled"
+import { fetchWorkouts } from "../../../API/workouts"
+import { AxiosResponse } from "axios"
 
 interface IMuscles {
     muscles: {
-        Arms: boolean;
-        Breast: boolean;
-        Chest: boolean;
-        Legs: boolean;
+        Arms: boolean
+        Breast: boolean
+        Chest: boolean
+        Legs: boolean
     }
 }
 
 const ItemList = ({ muscles }: IMuscles) => {
+    const queryWorkouts = useQuery("workouts", fetchWorkouts)
     const [minResOnPage, setMinResOnPage] = useState(0)
     const [maxResOnPage, setMaxResOnPage] = useState(6)
-    const matches = useMediaQuery('(min-width:2000px')
+    const matches = useMediaQuery("(min-width:2000px")
     const cardStyles = {
         width: !matches ? 220 : 320,
         height: !matches ? 202 : 302,
-        backgroundColor: '#F0F7FF',
-        margin: '36px 8px 0 8px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        cursor: 'pointer'
+        backgroundColor: "#F0F7FF",
+        margin: "36px 8px 0 8px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        cursor: "pointer",
     }
-    const changePage = (page) => {
+    const changePage = page => {
         setMinResOnPage(() => (page - 1) * 6)
         setMaxResOnPage(() => page * 6)
     }
     let filteredExercises = []
     const addOnArrResults = (list, partOfBody) => {
-        filteredExercises = [...filteredExercises,
-            ...list.filter(element => element.area === partOfBody)]
+        filteredExercises = [
+            ...filteredExercises,
+            ...list.filter(element => element.muscleGroup === partOfBody),
+        ]
     }
 
-    const filterExerciseList = (muscles, exerciseList) => {
-        muscles.Arms ? addOnArrResults(exerciseList, 'Arms') : null
-        muscles.Legs ? addOnArrResults(exerciseList, 'Legs') : null
-        muscles.Chest ? addOnArrResults(exerciseList, 'Chest') : null
-        muscles.Breast ? addOnArrResults(exerciseList, 'Breast') : null
+    const filterExerciseList = (muscles, list) => {
+        muscles.Arms ? addOnArrResults(list, "Arms") : null
+        muscles.Legs ? addOnArrResults(list, "Legs") : null
+        muscles.Chest ? addOnArrResults(list, "Chest") : null
+        muscles.Breast ? addOnArrResults(list, "Breast") : null
     }
-    filterExerciseList(muscles, exerciseList)
-
+    if (queryWorkouts.isSuccess) {
+        filterExerciseList(muscles, queryWorkouts.data.data.data)
+        console.log(queryWorkouts.data.data.data)
+    }
     const exercises = filteredExercises.map((item, index) => {
         if (index >= minResOnPage && index < maxResOnPage) {
             return (
-                <Link href={`/user/workoutList/workout/${item.id}`} key={item.id}>
+                <Link
+                    href={`/user/workoutList/workout/${item.id}`}
+                    key={item.id}
+                >
                     <Card sx={cardStyles}>
-                        <div><ImgWrapper
-                        imgUrl={item.img}
-                        imgWidth={item.imgWidth}
-                        imgHeight={item.imgHeight}
-                    /></div>
+                        <div>
+                            <ImgWrapper
+                                imgUrl={item.img}
+                                imgWidth={item.imgWidth}
+                                imgHeight={item.imgHeight}
+                            />
+                        </div>
                         <TextWrapper>
                             <Exercise>{item.name}</Exercise>
                             <Reps>{`${item.move} X ${item.repeat} REPS`}</Reps>
@@ -86,11 +96,14 @@ const ItemList = ({ muscles }: IMuscles) => {
             </Box>
             <Stack spacing={2} sx={{ margin: "10px 0 13px 0" }}>
                 <Pagination
-                    count={10}
+                    defaultPage={1}
+                    count={5}
+                    onChange={(e, value) => changePage(value)}
                     sx={{
                         display: "flex",
                         justifyContent: "flex-end",
                         marginRight: "30px",
+                        position: "absolute",
                     }}
                 />
             </Stack>
