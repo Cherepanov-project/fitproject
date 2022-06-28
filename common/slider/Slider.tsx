9fe47a05c542233514410741d43dd3f390c91904
Nikolay Shutikov <nikolayshutikov@gmail.com>
2022-06-28 22:04:33 +0700

    slider update

--- common/slider/Slider.tsx
+++ common/slider/Slider.tsx
@@ -1,67 +1,104 @@
-import React,{useEffect, useRef, useMemo, MouseEvent, useState} from "react"
-import { exerciseList } from "../../model/workout/workout"	
-import {CustomSlider, SliderWrapper, SliderSlide, SliderPagination, SliderPaginationBullet} from "./SliderStyle"
-let isFirefox;
+import React,{useEffect, useRef, useState, Children} from "react"
+import {CustomSlider, SliderWrapper, SliderSlide} from "./SliderStyle"
+
 let sliderIsDragging=false;
-let listener;
+let isMouseDown=false;
+let allCardsWidth;
+
+function useWindowSize() {
+  const [size, setSize] = useState(0);
+  useEffect(() => {
+    function updateSize() {
+      setSize(window.innerWidth);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
+
 const Slider = ({children}) => {
-  const [x,setDx] = useState([0,0])
+  const windowWidth = useWindowSize()
+  const [pairOfXs,setPairOfXs] = useState([0,0])
+  const [sliderWrapperWidth, setSliderWrapperWidth] = useState(0)
   const sliderWrapper = useRef(null)
+  const sliderSlide = useRef(null)
   
   useEffect(()=>{
-    isFirefox = window.navigator.userAgent.indexOf("Firefox") != -1
-    listener = document.addEventListener('mouseup',()=>{sliderIsDragging=false})
-    return ()=>{document.removeEventListener('mouseup', listener)}
-  },[])
+    let mouseUpDocHandler=(evt)=>{
+      console.log("document listener has worked")
+      isMouseDown=false
+      onPressedMouseOutsideTheSlider()
+    };
+    document.addEventListener('mouseup', mouseUpDocHandler)
+    return ()=>{document.removeEventListener('mouseup', mouseUpDocHandler)}
+  },[pairOfXs])
+
+  useEffect(()=>{
+    setSliderWrapperWidth(sliderWrapper.current?.clientWidth);
+    setPairOfXs([0,0]) 
+  },[windowWidth])
+
+  useEffect(()=>{
+    allCardsWidth = sliderSlide.current?.clientWidth*Children.count(children)
+    setSliderWrapperWidth(sliderWrapper.current?.clientWidth)
+  },[sliderSlide,sliderWrapper,children])
 
   function onSliderStartMove(evt){
     let curX=evt.clientX
-    let newPair=[]
-    newPair[0] = x[0] + curX
-    newPair[1] = curX
-    if(newPair[1]-newPair[0]<=0 && newPair[1]-newPair[0]>=(-sliderWrapper.current.clientWidth))
-      setDx(newPair)
+    let newPairOfXs=[]
+    newPairOfXs[0] = pairOfXs[0] + curX
+    newPairOfXs[1] = curX
+    if(newPairOfXs[1]-newPairOfXs[0]<=0)
+      if(allCardsWidth>sliderWrapperWidth)
+        if(Math.abs(newPairOfXs[1]-newPairOfXs[0])<allCardsWidth-sliderWrapperWidth)
+          setPairOfXs(newPairOfXs)
   }
+
   function onSliderMove(evt){
+    // debugger
     let curX=evt.clientX
-    let newPair=[]
-    newPair[0]=x[0]
-    newPair[1]=curX
-    if(newPair[1]-newPair[0]<=0 && newPair[1]-newPair[0]>=(-sliderWrapper.current.clientWidth))
-      setDx(newPair)
+    let newPairOfXs=[]
+    newPairOfXs[0]=pairOfXs[0]
+    newPairOfXs[1]=curX
+    if(newPairOfXs[1]-newPairOfXs[0]<=0 )
+    // первое условие- стоп на левой границе, второе и третье - на правой
+      if(allCardsWidth>sliderWrapperWidth)
+        if(Math.abs(newPairOfXs[1]-newPairOfXs[0])<allCardsWidth-sliderWrapperWidth)
+          setPairOfXs(newPairOfXs)      
   }
+
   function onSliderEndMove(evt){
     let curX=evt.clientX
-    let newPair=[]
-    newPair[0]=-(curX-x[0])
-    newPair[1]=0
-    if(newPair[1]-newPair[0]<=0){
-      if(!(newPair[1]-newPair[0]>=(-sliderWrapper.current.clientWidth))){
-        newPair[0]=-(x[1]-x[0])
-        newPair[1]=0
-        setDx(newPair)
+    let newPairOfXs=[]
+    newPairOfXs[0]=-(curX-pairOfXs[0])
+    // перекладываем разницу в начальный x для дальнейшего учёта начальной точки
+    newPairOfXs[1]=0
+    if(newPairOfXs[1]-newPairOfXs[0]<=0){
+      // разница newPairOfXs[1]-newPairOfXs[0] по модулю описывает насколько 
+      // необходимо передвинуть блок с карточками (упражнениями например) 
+      // знак указывает на направление передвижения
+      if((allCardsWidth>sliderWrapperWidth) && (Math.abs(newPairOfXs[1]-newPairOfXs[0])<allCardsWidth-sliderWrapperWidth)){
+        setPairOfXs(newPairOfXs)//добавляем новую ккординату
       }else{
-        setDx(newPair)
+        // не добавляем новую координату
+        // негативный случай, не двигаем, 
+        // просто перекладываем разницу в начальную точку
+        newPairOfXs[0]=-(pairOfXs[1]-pairOfXs[0])
+        newPairOfXs[1]=0
+        setPairOfXs(newPairOfXs)
       }
     } else{
-      setDx([0,0])
+      setPairOfXs([0,0])
     }
-    
   }
 
-  const onDragStartHandler=(evt)=>{
-    var img = new Image();
-    img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
-    evt.dataTransfer.setDragImage(img, 0, 0);
-    onSliderStartMove(evt);
-  }
-
-  const onDragHandler = (evt)=>{
-    onSliderMove(evt)
-  }
-  
-  const onDragEndHandler = (evt)=>{
-    onSliderEndMove(evt)
+  function onPressedMouseOutsideTheSlider(){
+    let newPairOfXs=[]
+    newPairOfXs[0]=-(pairOfXs[1]-pairOfXs[0])
+    newPairOfXs[1]=0
+    setPairOfXs(newPairOfXs)
   }
 
   const onTouchStartHandler=(evt)=>{
@@ -77,46 +114,64 @@ const Slider = ({children}) => {
   }
 
   const onMouseDown = (evt)=>{
-  
-    sliderIsDragging=true
+    evt.preventDefault()
+    isMouseDown=true
     onSliderStartMove(evt)
   }
+
   const onMouseMove = (evt)=>{
-    if(sliderIsDragging) onSliderMove(evt)
-  }
-  const onMouseUp = (evt)=>{
-    
-    sliderIsDragging=false
-    onSliderEndMove(evt)
+    evt.stopPropagation()
+    if(isMouseDown){
+      onSliderMove(evt)
+      sliderIsDragging=true
+    }
   }
   
-  const slides = React.Children.map(children, (child)=>{
-    return <SliderSlide>{child}</SliderSlide>
+  // const onMouseMoveCustomSlider = (evt) => {
+  //   console.log("onMouseMoveCustomSlider")
+  //   if(isMouseDown){
+      
+  //     isMouseDown=false
+  //     onPressedMouseOutsideTheSlider()
+  //   }
+  // }
+
+  const onMouseUp = (evt)=>{
+    isMouseDown=false
+    evt.stopPropagation()
+    onSliderEndMove(evt)
+  }
+
+  const onClick = (evt)=>{
+    if(sliderIsDragging){
+      evt.stopPropagation()
+      sliderIsDragging=false
+    }    
+  }
+  
+  const slides = React.Children.map(children, (child, index)=>{
+    if(index===0)
+      return <SliderSlide ref={sliderSlide}>{child}</SliderSlide>
+    else
+      return <SliderSlide>{child}</SliderSlide>
   })
+
   return(
       <CustomSlider>
         <SliderWrapper
-        draggable={isFirefox?false:true}
-        ref={sliderWrapper}
-        onDrag={(e)=>{if(!isFirefox) onDragHandler(e)}}
-        onDragStart={(evt)=>{if(!isFirefox) onDragStartHandler(evt)}}
-        onDragEnd={(evt)=>{if(!isFirefox) onDragEndHandler(evt)}}
-        onMouseDown={(e)=>{if(isFirefox) onMouseDown(e)}}
-        onMouseUp={(e)=>{if(isFirefox) onMouseUp(e)}}
-        onMouseMove={(e)=>{if(isFirefox) onMouseMove(e)}}
-        onTouchStart={(evt)=>{onTouchStartHandler(evt)}}
-        onTouchMove={(e)=>{onTouchHandler(e)}}
-        onTouchEnd={(evt)=>{onTouchEndHandler(evt)}}
-        style={{transform: `translateX(${(x[1]-x[0])}px)`, transitionDuration: '0ms'}}>
-          {slides}
+          className="className"
+          ref={sliderWrapper}
+          onDragStart={()=>{return false}}
+          onMouseDown={(e)=>{ onMouseDown(e)}}
+          onMouseUpCapture={(e)=>{ onMouseUp(e)}}
+          onClickCapture={(e)=>{ onClick(e)}}
+          onMouseMove={(e)=>{ onMouseMove(e)}}
+          onTouchStart={(evt)=>{onTouchStartHandler(evt)}}
+          onTouchMove={(e)=>{onTouchHandler(e)}}
+          onTouchEnd={(evt)=>{onTouchEndHandler(evt)}}          
+          style={{transform: `translateX(${(pairOfXs[1]-pairOfXs[0])}px)`, transitionDuration: '0ms'}}>
+          {slides}         
         </SliderWrapper>
-        {/* <SliderPagination>
-          <SliderPaginationBullet></SliderPaginationBullet>
-          <SliderPaginationBullet></SliderPaginationBullet>
-          <SliderPaginationBullet></SliderPaginationBullet>
-          <SliderPaginationBullet></SliderPaginationBullet>
-          <SliderPaginationBullet></SliderPaginationBullet>
-        </SliderPagination> */}
       </CustomSlider>
   )
 }
