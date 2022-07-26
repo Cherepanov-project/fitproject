03ccde7df35a23ede980204fb7541f72aecdaf03
Bugagych <ansahno@mail.ru>
2022-07-26 21:10:12 +0300

    fix

--- components/PageComponents/PageUser/pageUser.styles.ts
+++ components/PageComponents/PageUser/pageUser.styles.ts
@@ -1,68 +1,69 @@
 import styled from "styled-components"
 
 export const ContentWrapper = styled.div`
-    width: 100%;
-    border: 1px solid #dfe0eb;
-    border-radius: 8px;
-    margin: 0 auto;
-    background-color: #ffffff;
+  width: 100%;
+  border: 1px solid #dfe0eb;
+  border-radius: 8px;
+  margin: 0 auto;
+  background-color: #ffffff;
 `
 
 export const Footer = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 `
 
 export const ContentWrapperUser = styled.div`
-    padding: 20px;
-    border: 1px solid #dfe0eb;
-    border-radius: 8px;
-    background-color: #ffffff;
+  padding: 20px;
+  border: 1px solid #dfe0eb;
+  border-radius: 8px;
+  background-color: #ffffff;
 `
 
 export const TitleHeader = styled.h4`
-    font-weight: 400;
-    font-size: 21px;
-    line-height: 24px;
-    color: #252733;
+  font-weight: 400;
+  font-size: 21px;
+  line-height: 24px;
+  color: #252733;
 `
 
 export const UsersContainer = styled.div`
-    display: flex;
-    align-items: center;
-    border-top: 1px solid #dfe0eb;
-    border-bottom: 1.5px solid #dfe0eb;
-    height: 80px;
+  display: flex;
+  align-items: center;
+  border-top: 1px solid #dfe0eb;
+  border-bottom: 1.5px solid #dfe0eb;
+  height: 80px;
 `
 
 export const UserName = styled.p`
-    font-weight: 700;
-    font-size: 21px;
-    line-height: 24px;
-    color: #6e41e2;
+  font-weight: 700;
+  font-size: 21px;
+  line-height: 24px;
+  color: #6e41e2;
 `
 export const Title = styled.h3`
-    font-weight: 700;
-    border-bottom: 1px solid #dfe0eb;
-    padding: 15px 0;
-    margin: 0;
+  font-weight: 700;
+  border-bottom: 1px solid #dfe0eb;
+  padding: 15px 0;
+  margin: 0;
 `
 export const InfoItem = styled.div`
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #dfe0eb;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #dfe0eb;
 `
 export const TextInfo = styled.p`
-    width: 150px;
-    color: rgba(17, 17, 17, 0.48);
+  width: 150px;
+  color: rgba(17, 17, 17, 0.48);
+  line-height: 35px;
 `
 export const BtnContainer = styled.div`
-    display: flex;
-    padding-top: 20px;
+  display: flex;
+  padding-top: 20px;
 `
 
 export const StyledLink = styled.a`
-    text-decoration: none;
-    color: green;
+  text-decoration: none;
+  color: green;
 `
 // color: ${({ color }) => (color ? color : `rgba(17, 17, 17, 0.48)`)};
--- components/PageComponents/PageUser/pageUser.tsx
+++ components/PageComponents/PageUser/pageUser.tsx
@@ -16,15 +16,15 @@ import ColorfulTeg from "../../ColorfulTeg"
 
 // styles
 import {
-    ContentWrapperUser,
-    TitleHeader,
-    UsersContainer,
-    UserName,
-    Title,
-    InfoItem,
-    TextInfo,
-    BtnContainer,
-    StyledLink,
+  ContentWrapperUser,
+  TitleHeader,
+  UsersContainer,
+  UserName,
+  Title,
+  InfoItem,
+  TextInfo,
+  BtnContainer,
+  StyledLink,
 } from "./pageUser.styles"
 import { useEffect, useState } from "react"
 
@@ -39,112 +39,96 @@ import { initalStateUser } from "@/models/user/user"
 import { getQueryStringParams } from "@/utils/subscribePageName"
 
 const PageUser = props => {
-    const router = useRouter()
+  const router = useRouter()
 
-    const [data, setData] = useState(initalStateUser)
+  const [data, setData] = useState(initalStateUser)
 
-    useEffect(() => {
-        const { data } = getQueryStringParams(decodeURI(window.location.search))
+  useEffect(() => {
+    const { data } = getQueryStringParams(decodeURI(window.location.search))
 
-        setData(() => JSON.parse(data))
-    }, [])
+    setData(() => JSON.parse(data))
+  }, [])
 
-    const deleteUser = async () => {
-        const response = await deleteUserById(data.id)
-        router.back()
-    }
+  const deleteUser = async () => {
+    const response = await deleteUserById(data.id)
+    router.back()
+  }
 
-    return (
-        <ContentWrapperUser>
-            <TitleHeader>(Role) Profile</TitleHeader>
-            <UsersContainer>
-                <Avatar
-                    sx={{ height: "50px", width: "50px", marginRight: "10px" }}
-                >
-                    <Image src={avatarUser} alt="avatar" />
-                </Avatar>
-                <UserName>Alex</UserName>
-            </UsersContainer>
-            <Title>User information</Title>
-            <div className="InfoList">
-                <InfoItem>
-                    <TextInfo>User Name</TextInfo>
-                    <span>{data.username}</span>
-                </InfoItem>
-                <InfoItem>
-                    <TextInfo>Name</TextInfo>
-                    <span>
-                        {data.firstName
-                            ? data.firstName + data.lastName
-                            : "Name is not specified"}
-                    </span>
-                </InfoItem>
-                <InfoItem>
-                    <TextInfo>Email</TextInfo>
-                    <span>
-                        {data.email ? data.email : "Email is not specified"}
-                    </span>
-                </InfoItem>
-                <InfoItem>
-                    <TextInfo>Phone</TextInfo>
-                    <span>
-                        {data.phone ? data.phone : "Phone is not specified"}
-                    </span>
-                </InfoItem>
-                <InfoItem>
-                    <TextInfo>Age</TextInfo>
-                    <span>{data.age ? data.age : "Age is not specified"}</span>
-                </InfoItem>
-                <InfoItem>
-                    <TextInfo>Gender</TextInfo>
-                    <span>
-                        {data.gender ? data.gender : "Gender is not specified"}
-                    </span>
-                </InfoItem>
-                <InfoItem>
-                    <TextInfo>Role</TextInfo>
-                    {data.coach ? (
-                        <ColorfulTeg backgroundColor="#F12B2C" text="admin" />
-                    ) : (
-                        <ColorfulTeg text="user" />
-                    )}
-                </InfoItem>
-            </div>
-            <BtnContainer>
-                <Link href={`/admin/users/`} passHref>
-                    <Button variant="outlined" startIcon={<ArrowBackIcon />}>
-                        Back
-                    </Button>
-                </Link>
-                <Button
-                    onClick={deleteUser}
-                    sx={{ margin: "0 30px" }}
-                    variant="outlined"
-                    color="error"
-                    startIcon={<DeleteIcon />}
-                >
-                    delete
-                </Button>
+  return (
+    <ContentWrapperUser>
+      <TitleHeader>(Role) Profile</TitleHeader>
+      <UsersContainer>
+        <Avatar sx={{ height: "50px", width: "50px", marginRight: "10px" }}>
+          <Image src={avatarUser} alt="avatar" />
+        </Avatar>
+        <UserName>{data.username}</UserName>
+      </UsersContainer>
+      <Title>User information</Title>
+      <div className="InfoList">
+        <InfoItem>
+          <TextInfo>Name</TextInfo>
+          <span>
+            {data.firstName
+              ? data.firstName + data.lastName
+              : "Name is not specified"}
+          </span>
+        </InfoItem>
+        <InfoItem>
+          <TextInfo>Email</TextInfo>
+          <span>{data.email ? data.email : "Email is not specified"}</span>
+        </InfoItem>
+        <InfoItem>
+          <TextInfo>Phone</TextInfo>
+          <span>{data.phone ? data.phone : "Phone is not specified"}</span>
+        </InfoItem>
+        <InfoItem>
+          <TextInfo>Age</TextInfo>
+          <span>{data.age ? data.age : "Age is not specified"}</span>
+        </InfoItem>
+        <InfoItem>
+          <TextInfo>Gender</TextInfo>
+          <span>{data.gender ? data.gender : "Gender is not specified"}</span>
+        </InfoItem>
+        <InfoItem>
+          <TextInfo>Role</TextInfo>
+          {data.coach ? (
+            <ColorfulTeg backgroundColor="#F12B2C" text="admin" />
+          ) : (
+            <ColorfulTeg text="user" />
+          )}
+        </InfoItem>
+      </div>
+      <BtnContainer>
+        <Link href={`/admin/users/`} passHref>
+          <Button variant="outlined" startIcon={<ArrowBackIcon />}>
+            Back
+          </Button>
+        </Link>
+        <Button
+          onClick={deleteUser}
+          sx={{ margin: "0 30px" }}
+          variant="outlined"
+          color="error"
+          startIcon={<DeleteIcon />}
+        >
+          delete
+        </Button>
 
-                <Link
-                    href={{
-                        pathname: `/admin/users/edit-form/${data.id}`,
-                        query: {
-                            data: JSON.stringify(data),
-                        },
-                    }}
-                    passHref
-                >
-                    <Button
-                        variant="outlined"
-                        startIcon={<EditIcon />}
-                        color="success"
-                    >
-                        Edit
-                    </Button>
-                </Link>
-            </BtnContainer>
-        </ContentWrapperUser>
-    )
+        <Link
+          href={{
+            pathname: `/admin/users/edit-form/${data.id}`,
+            query: {
+              data: JSON.stringify(data),
+            },
+          }}
+          passHref
+        >
+          <Button variant="outlined" startIcon={<EditIcon />} color="success">
+            Edit
+          </Button>
+        </Link>
+      </BtnContainer>
+    </ContentWrapperUser>
+  )
 }
 export default PageUser
