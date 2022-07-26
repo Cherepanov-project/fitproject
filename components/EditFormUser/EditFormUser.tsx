a3f60a66ddda1263d8bcaea84088c4cb7fd5b2d4
Bugagych <63162914+Bugagych@users.noreply.github.com>
2022-07-26 18:55:27 +0300

    Update EditFormUser.tsx

--- components/EditFormUser/EditFormUser.tsx
+++ components/EditFormUser/EditFormUser.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react"
-import Link from "next/link"
 import { useRouter } from "next/router"
 
 // components
@@ -15,7 +14,6 @@ import {
 
 // ui libs
 import { Field, Formik, Form, FormikProps } from "formik"
-
 import ArrowBackIcon from "@mui/icons-material/ArrowBack"
 
 // utils
@@ -30,6 +28,7 @@ import { IEditFormUser } from "@/components/EditFormUser/EditFormUser.interface"
 
 // model
 import { initalStateUser } from "@/models/user/user"
+import { addFormModel } from "@/models/admin/add-form/addForm.model"
 
 const EditFormUser = ({ title, button }) => {
     const router = useRouter()
@@ -49,15 +48,19 @@ const EditFormUser = ({ title, button }) => {
     })
 
     const back = () => {
-        setTimeout(() => {
-            router.push({
-                pathname: `/admin/users/${data.id}`,
-                query: {
-                    data: JSON.stringify(data),
-                },
-            }),
-                1000
-        })
+        if (title !== "Add user") {
+            setTimeout(() => {
+                router.push({
+                    pathname: `/admin/users/${data.id}`,
+                    query: {
+                        data: JSON.stringify(data),
+                    },
+                }),
+                    1000
+            })
+        } else {
+            router.back()
+        }
     }
 
     return (
@@ -74,15 +77,7 @@ const EditFormUser = ({ title, button }) => {
                 validationSchema={validationCreateUpdateUser}
                 enableReinitialize={true}
                 initialValues={{
-                    id: data.id,
-                    username: data.username,
-                    password: "",
-                    firstName: data.firstName,
-                    lastName: data.lastName,
-                    email: data.email,
-                    phone: data.phone,
-                    age: data.age,
-                    gender: data.gender,
+                    ...data,
                 }}
                 onSubmit={async (values, { resetForm }) => {
                     const response =
@@ -94,6 +89,7 @@ const EditFormUser = ({ title, button }) => {
                         page === "add-form"
                             ? "The user is registered successfully."
                             : "The user is update successfully."
+
                     if (response.success === false) {
                         setMessage({ message: response.error, success: false })
                     } else {
@@ -104,7 +100,6 @@ const EditFormUser = ({ title, button }) => {
                         if (page !== "add-form") {
                             setData(() => values)
                         }
-
                         resetForm()
                     }
                 }}
@@ -112,123 +107,50 @@ const EditFormUser = ({ title, button }) => {
                 {({ errors, touched }) => (
                     <StyleForm>
                         <div>
-                            <label htmlFor="username">
-                                <span>Username</span>
-                                <Field
-                                    type="text"
-                                    name="username"
-                                    id="username"
-                                    placeholder="User name"
-                                />
-                                {errors.username && touched.username ? (
-                                    <span className="error">
-                                        {errors.username}
-                                    </span>
-                                ) : null}
-                            </label>
-
-                            <label htmlFor="password">
-                                <span>Password</span>
-                                <Field
-                                    type="text"
-                                    id="password"
-                                    name="password"
-                                    placeholder="Password"
-                                />
-                                {errors.password && touched.password ? (
-                                    <span className="error">
-                                        {errors.password}
-                                    </span>
-                                ) : null}
-                            </label>
+                            {addFormModel.map(item => {
+                                if (item.type !== "select") {
+                                    return (
+                                        <label
+                                            key={item.label}
+                                            htmlFor={item.label}
+                                        >
+                                            <span>{item.span}</span>
+                                            <Field
+                                                type={item.type}
+                                                name={item.label}
+                                                id={item.label}
+                                                placeholder={item.span}
+                                            />
+                                            {errors[item.label] &&
+                                            touched[item.label] ? (
+                                                <span className="error">
+                                                    {errors[item.label]}
+                                                </span>
+                                            ) : null}
+                                        </label>
+                                    )
+                                } else {
+                                    return (
+                                        <label htmlFor={item.label}>
+                                            <span>{item.span}</span>
+                                            <Field
+                                                as={item.type}
+                                                name={item.label}
+                                            >
+                                                <option value=""></option>
+                                                <option value="MALE">
+                                                    Male
+                                                </option>
+                                                <option value="FEMALE">
+                                                    Female
+                                                </option>
+                                            </Field>
+                                        </label>
+                                    )
+                                }
+                            })}
                         </div>
-                        <div>
-                            <label htmlFor="firstName">
-                                <span>First name</span>
-                                <Field
-                                    id="firstName"
-                                    type="text"
-                                    name="firstName"
-                                    placeholder="First name"
-                                />
-                                {errors.firstName && touched.firstName ? (
-                                    <span className="error">
-                                        {errors.firstName}
-                                    </span>
-                                ) : null}
-                            </label>
-                            <label htmlFor="lastName">
-                                <span>Last name</span>
-                                <Field
-                                    id="lastName"
-                                    type="text"
-                                    name="lastName"
-                                    placeholder="Last name"
-                                />
-                                {errors.lastName && touched.lastName ? (
-                                    <span className="error">
-                                        {errors.lastName}
-                                    </span>
-                                ) : null}
-                            </label>
-                        </div>
-                        <div>
-                            <label htmlFor="email">
-                                <span>Email address</span>
-                                <Field
-                                    type="email"
-                                    name="email"
-                                    placeholder="Email"
-                                />
-                                {errors.email && touched.email ? (
-                                    <span className="error">
-                                        {errors.email}
-                                    </span>
-                                ) : null}
-                            </label>
-                            <label htmlFor="phone">
-                                <span>Phone</span>
-                                <Field
-                                    id="phone"
-                                    type="phone"
-                                    name="phone"
-                                    placeholder="Phone"
-                                />
-                                {errors.phone && touched.phone ? (
-                                    <span className="error">
-                                        {errors.phone}
-                                    </span>
-                                ) : null}
-                            </label>
-                        </div>
-                        <div>
-                            <label htmlFor="age">
-                                <span>Age</span>
-                                <Field
-                                    id="age"
-                                    type="number"
-                                    name="age"
-                                    placeholder="Age"
-                                />
-                                {errors.age && touched.age ? (
-                                    <span className="error">{errors.age}</span>
-                                ) : null}
-                            </label>
-                            <label htmlFor="gender">
-                                <span>Gender</span>
-                                <Field as="select" name="gender">
-                                    <option value=""></option>
-                                    <option value="MALE">Male</option>
-                                    <option value="FEMALE">Female</option>
-                                </Field>
-                                {errors.gender && touched.gender ? (
-                                    <span className="error">
-                                        {errors.gender}
-                                    </span>
-                                ) : null}
-                            </label>
-                        </div>
-                        <div>
+                        <div className="block-button">
                             <StyleBackButton
                                 onClick={back}
                                 variant="outlined"
@@ -236,7 +158,6 @@ const EditFormUser = ({ title, button }) => {
                             >
                                 Back
                             </StyleBackButton>
-
                             <label htmlFor="">
                                 <input type="submit" value={button} />
                             </label>
