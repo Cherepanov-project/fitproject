88356072a2c7017f2150d35cd9dedbade69b3dcb
Bugagych <ansahno@mail.ru>
2022-07-26 20:35:13 +0300

    fix

--- containers/Layout-user/Navbar/leftSideBar.tsx
+++ containers/Layout-user/Navbar/leftSideBar.tsx
@@ -1,19 +1,18 @@
 import Link from "next/link"
-import {useRouter} from "next/router";
+import { useRouter } from "next/router"
 import Image from "next/image"
 import Box from "@mui/material/Box"
 import { Drawer } from "@mui/material"
 
 // styles
 import {
-    Sidebar,
-    SidebarWrapper,
-    IconListWrapper,
-    TopIconsWrapper,
-    BottomIconWrapper,
-
-    ImageWrapper,
-    Divider,
+  Sidebar,
+  SidebarWrapper,
+  IconListWrapper,
+  TopIconsWrapper,
+  BottomIconWrapper,
+  ImageWrapper,
+  Divider,
 } from "./leftSideBar.styles"
 
 // models
@@ -23,42 +22,37 @@ import { sidebarMenu } from "@/models/user/layout/sidebarModel"
 import help from "@/common/images/layoutUser/navbarIcons/help.svg"
 
 const LeftSideBar = () => {
+  const router = useRouter()
+  const page = router.asPath.split("/").pop()
 
-    const router = useRouter()
-    const page = router.asPath.split("/").pop()
-
-    const menuList = sidebarMenu.map(item => {
-        return (
-            <>
-                <Link href={item.route} passHref>
-                    <ImageWrapper selected={item.name === page}>
-                        <Image src={item.icon} alt={item.name} width="22px"
-                               height="22px"/>
-                    </ImageWrapper>
-                </Link>
-                {item?.divider ? <Divider /> : ""}
-            </>
-        )
-    })
+  const menuList = sidebarMenu.map(item => {
     return (
-        <SidebarWrapper>
-            <Sidebar>
-                <IconListWrapper>
-                    <TopIconsWrapper>
-                        {menuList}
-                    </TopIconsWrapper>
-                    <BottomIconWrapper>
-                        <Link href={"#"} passHref>
-                            <ImageWrapper selected={'help' === page} >
-                                <Image src={help.src} alt="h" width="24"
-                                       height="24"/>
-                            </ImageWrapper>
-                        </Link>
-                    </BottomIconWrapper>
-                </IconListWrapper>
-            </Sidebar>
-        </SidebarWrapper>
+      <>
+        <Link href={item.route} passHref>
+          <ImageWrapper selected={item.name === page}>
+            <Image src={item.icon} alt={item.name} width="22px" height="22px" />
+          </ImageWrapper>
+        </Link>
+        {item?.divider ? <Divider /> : ""}
+      </>
     )
+  })
+  return (
+    <SidebarWrapper>
+      <Sidebar>
+        <IconListWrapper>
+          <TopIconsWrapper>{menuList}</TopIconsWrapper>
+          <BottomIconWrapper>
+            <Link href={"#"} passHref>
+              <ImageWrapper selected={"help" === page}>
+                <Image src={help.src} alt="h" width="24" height="24" />
+              </ImageWrapper>
+            </Link>
+          </BottomIconWrapper>
+        </IconListWrapper>
+      </Sidebar>
+    </SidebarWrapper>
+  )
 }
 
 export default LeftSideBar
