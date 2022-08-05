b49b954ce6a0f98b62d3bd71fd17792864d3d020
igp2000 <74302129+igp2000@users.noreply.github.com>
2022-08-05 17:01:42 +0400

    fix

--- utils/fonts/fontStyles.ts
+++ utils/fonts/fontStyles.ts
@@ -1,18 +1,21 @@
 import { createGlobalStyle } from "styled-components"
 
+// Файлы шрифтов лежат в папке public сайта
+// поэтому для реального сайта пути понадобится исправить
+
 export const FontStyles = createGlobalStyle`
 @font-face {
   font-family: 'Mulish';
   font-style: normal;
   font-weight: 400;
-  src: url('/fonts/mulish-v10-latin/mulish-v10-latin-regular.eot');
+  src: url('/fitproject/fonts/mulish-v10-latin/mulish-v10-latin-regular.eot');
 }
 `
 export const FontRoboto = createGlobalStyle`
 @font-face {
     font-family: 'Roboto';
     src: local('Roboto')
-         url('/fonts/roboto/Roboto-Regular.ttf') format('truetype');
+         url('/fitproject/fonts/roboto/Roboto-Regular.ttf') format('truetype');
     font-weight: 400;
     font-style: normal;
 }
@@ -20,7 +23,7 @@ export const FontRoboto = createGlobalStyle`
 @font-face {
     font-family: 'Roboto';
     src: local('Roboto Italic'), local('Roboto-Italic'), 
-         url('/fonts/roboto/Roboto-Italic.ttf') format('truetype');
+         url('/fitproject/fonts/roboto/Roboto-Italic.ttf') format('truetype');
     font-weight: 400;
     font-style: italic;
 }
@@ -28,9 +31,9 @@ export const FontRoboto = createGlobalStyle`
 @font-face {
     font-family: 'Roboto';
     src: local('Roboto Medium'), local('Roboto-Medium'), 
-         url('/fonts/roboto/Robotomedium.woff2') format('woff2'), 
-         url('/fonts/roboto/Robotomedium.woff') format('woff'), 
-         url('/fonts/roboto/Robotomedium.ttf') format('truetype');
+         url('/fitproject/fonts/roboto/Robotomedium.woff2') format('woff2'), 
+         url('/fitproject/fonts/roboto/Robotomedium.woff') format('woff'), 
+         url('/fitproject/fonts/roboto/Robotomedium.ttf') format('truetype');
     font-weight: 500;
     font-style: normal;
 }
@@ -38,9 +41,9 @@ export const FontRoboto = createGlobalStyle`
 @font-face {
     font-family: 'Roboto';
     src: local('Roboto Medium Italic'), local('Roboto-MediumItalic'), 
-         url('/fonts/roboto/Robotomediumitalic.woff2') format('woff2'), 
-         url('/fonts/roboto/Robotomediumitalic.woff') format('woff'), 
-         url('/fonts/roboto/Robotomediumitalic.ttf') format('truetype');
+         url('/fitproject/fonts/roboto/Robotomediumitalic.woff2') format('woff2'), 
+         url('/fitproject/fonts/roboto/Robotomediumitalic.woff') format('woff'), 
+         url('/fitproject/fonts/roboto/Robotomediumitalic.ttf') format('truetype');
     font-weight: 500;
     font-style: italic;
 }
@@ -50,7 +53,7 @@ export const FontPoppins = createGlobalStyle`
 @font-face {
     font-family: 'Poppins';
     src: local('Poppins Medium'), local('Poppins-Medium'), 
-         url('/fonts/poppins/Poppins-Medium.ttf') format('truetype');
+         url('/fitproject/fonts/poppins/Poppins-Medium.ttf') format('truetype');
     font-weight: 600;
     font-style: normal;
 }
@@ -58,7 +61,7 @@ export const FontPoppins = createGlobalStyle`
 @font-face {
     font-family: 'Poppins';
     src: local('Poppins Medium Italic'), local('Poppins-MediumItalic'), 
-         url('/fonts/poppins/Poppins-MediumItalic.ttf') format('truetype');
+         url('/fitproject/fonts/poppins/Poppins-MediumItalic.ttf') format('truetype');
     font-weight: 600;
     font-style: italic;
 }
@@ -68,7 +71,7 @@ export const FontOpenSans = createGlobalStyle`
 @font-face {
     font-family: 'Open Sans';
     src: local('Open Sans'), local('Open-Sans'), 
-         url('/fonts/open_sans/OpenSans-Regular.ttf') format('truetype');
+         url('/fitproject/fonts/open_sans/OpenSans-Regular.ttf') format('truetype');
     font-weight: 400;
     font-style: normal;
 }
@@ -76,7 +79,7 @@ export const FontOpenSans = createGlobalStyle`
 @font-face {
     font-family: 'Open Sans';
     src: local('Open Sans Italic'), local('Open-SansItalic'), 
-         url('/fonts/open_sans/OpenSans-Italic.ttf') format('truetype');
+         url('/fitproject/fonts/open_sans/OpenSans-Italic.ttf') format('truetype');
     font-weight: 400;
     font-style: italic;
 }
@@ -87,7 +90,7 @@ export const FontArimaMadurai = createGlobalStyle`
 @font-face {
     font-family: 'Arima Madurai';
     src: local('Arima Madurai'), local('Arima-Madurai'), 
-         url('/fonts/arima_madurai/ArimaMadurai-ExtraBold.ttf') format('truetype');
+         url('/fitproject/fonts/arima_madurai/ArimaMadurai-ExtraBold.ttf') format('truetype');
     font-weight: 800;
     font-style: Extrabold;
 }
