6e681563c8ce8c07f91932d057c5487618e44860
Aipov Shamil <sh.xphnx@gmail.com>
2022-08-10 13:51:11 +0300

    Added LayutUser to FooddishItem

--- pages/user/calendar/dietaplan/[food].tsx
+++ pages/user/calendar/dietaplan/[food].tsx
@@ -1,12 +1,14 @@
 import CalendarContainer from "@/components/Calendar/calendarContainer"
 import Dish from "@/components/Dish/dish"
+import { LayoutUser } from "@/containers/Layout-user/layoutUser"
+import WithRefreshingToken from "@/containers/Layout-user/WithRefreshingToken"
 
 const FoodDishItem = () => {
-    return (
-        <CalendarContainer>
-            <Dish />
-        </CalendarContainer>
-    )
+  return (
+    <CalendarContainer>
+      <Dish />
+    </CalendarContainer>
+  )
 }
 
-export default FoodDishItem
+export default WithRefreshingToken(LayoutUser(FoodDishItem))
