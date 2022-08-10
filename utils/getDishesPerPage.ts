e62347a1d45b77abd62c43826ac39be53789b22f
Aipov Shamil <sh.xphnx@gmail.com>
2022-08-10 12:56:00 +0300

    Displaying one dish was replaced by displaying two dishes

--- utils/getDishesPerPage.ts
+++ utils/getDishesPerPage.ts
@@ -1,7 +1,7 @@
 export function getDishesPerPage() {
   if (typeof window !== "undefined") {
     const { innerWidth } = window
-    let dishes = innerWidth < 1260 ? 1 : innerWidth < 1510 ? 4 : 6
+    let dishes = innerWidth < 1260 ? 2 : innerWidth < 1510 ? 4 : 6
     return dishes
   }
 }
