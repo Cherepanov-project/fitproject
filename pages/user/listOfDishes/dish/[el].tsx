9071f9cc75d1582a1e3bd55b76b494488956c645
Шамиль Аипов <32361252+xphnx@users.noreply.github.com>
2022-08-10 19:24:38 +0300

    Replaced urlQuery by (urlQuery || '')

--- pages/user/listOfDishes/dish/[el].tsx
+++ pages/user/listOfDishes/dish/[el].tsx
@@ -29,7 +29,7 @@ const DishesItem = (): JSX.Element => {
 
   const filteredListDishes = dishFoodAll.filter(item => {
     const star = specificationStar.filter(s => s.id === item.star)[0]
-    return urlQuery.includes(item.id) && urlQuery.includes(star.name)
+    return (urlQuery || '').includes(item.id) && (urlQuery || '').includes(star.name)
   })
 
   const listDishes = filteredListDishes.map(item => (
