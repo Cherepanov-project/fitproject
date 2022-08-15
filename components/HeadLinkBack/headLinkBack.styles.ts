c3218f553015bd00faf83a079553aacc55800639
Aipov Shamil <xphnx@ya.ru>
2022-08-15 18:00:22 +0300

    Component HeadLinkBack was corrected and added to page of a separate dish that has route from calendar

--- components/HeadLinkBack/headLinkBack.styles.ts
+++ components/HeadLinkBack/headLinkBack.styles.ts
@@ -1,18 +1,27 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const DietaHead = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-left: 10%;
-    margin-right: 10%;
-    font-size: 18px;
-    font-weight: bold;
-    padding-top: 20px;
+  font-family: "Roboto";
+  display: flex;
+  justify-content: space-between;
+  margin-left: 55px;
+  margin-right: 35px;
+  font-size: 18px;
+  font-weight: bold;
+  padding-top: 20px;
 `
 
 export const HeadLink = styled.a`
-    text-decoration: none;
-    color: black;
-    cursor: pointer;
-    word-spacing: 12px;
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+  word-spacing: 12px;
+`
+
+export const StyledHeadText = styled.span`
+  word-spacing: normal;
+  font-weight: normal;
+`
+export const StyledNameComponent = styled.div`
+  color: #344767;
 `
