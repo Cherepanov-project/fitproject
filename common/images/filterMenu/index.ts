6dddd384dd917ffdda49f66ce9f845db7e64a8b2
ninelamericanel <ninelamericanel@gmail.com>
2022-08-26 12:34:45 +0300

    added new pictures for sorting

--- common/images/filterMenu/index.ts
+++ common/images/filterMenu/index.ts
@@ -1,2 +1,4 @@
 export const imageFilter = require("./filter.svg")
 export const imageSort = require("./sort.svg")
+export const imageDescending = require("./descending.svg")
+export const imageAscending = require("./ascending.svg")
