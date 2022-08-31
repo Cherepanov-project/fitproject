73d534a4ccf7a80c6b7fd4b2b75bcc88e419b745
ninelamericanel <ninelamericanel@gmail.com>
2022-08-31 11:46:57 +0300

    moved state from component to global component state

--- components/Checkbox/checkbox.interfaces.ts
+++ components/Checkbox/checkbox.interfaces.ts
@@ -2,6 +2,6 @@ import {ChangeEvent} from "react";
 
 export interface Props {
     checkbox: string;
-    handleSubFilter: (event?: ChangeEvent<HTMLInputElement>) => void;
-    sort?: boolean;
+    handleSubFilter: (event: ChangeEvent<HTMLInputElement>, isChecked: boolean) => void;
+    isChecked: boolean;
 }
--- components/Checkbox/index.tsx
+++ components/Checkbox/index.tsx
@@ -2,18 +2,15 @@ import React, { ChangeEvent, FC, useState } from "react"
 import { StyledSubInput} from "@/components/Filter/filter.styles"
 import { Props } from "./checkbox.interfaces"
 
-const Checkbox: FC<Props> = ({checkbox, handleSubFilter}) => {
-    const [isChecked, setIsChecked] = useState<boolean>(false)
-    // const [noSort, setNoSort] = useState(true);
+const Checkbox: FC<Props> = ({isChecked, checkbox, handleSubFilter}) => {
 
     const handleClick = (event: ChangeEvent<HTMLInputElement>) => {
-        if (isChecked) {
-            handleSubFilter()
+        if (!isChecked) {
+            handleSubFilter(event, true)
+        } else {
+            handleSubFilter(event, false)
         }
-        setIsChecked(!isChecked)
-        handleSubFilter(event)
     }
-    console.log(checkbox);
 
     return (
         <StyledSubInput type="checkbox" name={checkbox} checked={isChecked} onChange={(event) => handleClick(event)}
