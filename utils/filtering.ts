02b2a49dfac0c7490bb145d2adb972e64eb7c94c
ninelamericanel <ninelamericanel@gmail.com>
2022-09-06 16:31:21 +0300

    fixed filtering function

--- utils/filtering.ts
+++ utils/filtering.ts
@@ -1,76 +1,18 @@
-// export const filtering = (array, filterBy, subFilter) => {
-//     const val = isNaN(parseInt(subFilter)) ? subFilter : parseInt(subFilter)
-//     // if (typeof val === 'string') {
-//     //     let filter = array.filter(element => {
-//     //         return element[filterBy.toLowerCase()] == val
-//     //     })
-//     //     return filter
-//     // } else {
-//         let filter = array.filter(element => {
-//             return element[filterBy.toLowerCase()] < val
-//         })
-//         return filter
-//     // }
-// }
-
-export const filtering = (array, {filter, sort}) => {
-    let filteredOptions = new Map();
-    let arrayOpt = [];
-
-    for (let key in filter) {
-        for (let options in filter[key]) {
-            if (filter[key][options]) {
-                arrayOpt.push(options.toUpperCase());
-                filteredOptions.set(key.toLowerCase(), arrayOpt)
+export const filtering = (array, filters) => {
+    return array.reduce((acc, item) => {
+        let isFitArr: boolean[] = []
+        filters.forEach(([key, values])=> {
+            if (typeof values[0] === 'number') {
+                isFitArr.push(item[key] >= values[0] && item[key] <= values[1])
+            } else {
+                isFitArr.push(values.includes(item[key]))
             }
-        }
-    }
-
-    if (filteredOptions.size) {
-        return array.filter(item => {
-            return [...filteredOptions].every(([key, values]) => {
-                    return values.includes(item[key])
-                }
-            )
         })
-    }
 
-    return array;
+        if (!isFitArr.includes(false)) {
+            acc.push(item);
+        }
 
-
-    // const filtered = array.filter(o => {
-    //     filterArr.every(([key, values]) => {
-    //         o[]
-    //     })
-    // })
-    // if (filtersArr) {
-    // return array.filter(o =>
-    //     filtersArr.every(([key, values]) => {
-    //         if (o[key.toLowerCase()]) {
-    //
-    //         }
-    //         // return o[key.toLowerCase()]  <= +values[1] && o[key.toLowerCase()] >= +values[0]
-    //     })
-    // )}
-
-    // const filteredValues = filtersArr.filter(([key, values]) => Object.entries(values).includes(true, 1))
-
-    // array.filter((item) => {
-        // console.log(item)
-    // })
-    // const filterValues = filtersArr.filter(([key, values]) => {
-    //     const valuesArr = Object.entries(values);
-    //
-    // })
-    // console.log(filtersArr);
-    // const gavno = array.filter(o => {
-    //     filtersArr.every(([key, values]) => {
-    //         let filtredValues = Object.keys(values).filter((el) => values[el])
-    //         let valuesToUpperCase = filtredValues.map((el) => el.toUpperCase());
-    //         // console.log(valuesToUpperCase.includes(o[key.toLowerCase()]));
-    //         return valuesToUpperCase.includes(o[key.toLowerCase()])
-    //     })
-    // })
-
-    // return gavno
+        return acc;
+    }, [])
 }
