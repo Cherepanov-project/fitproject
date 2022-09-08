a41b65ae25f1d7acb8a82a148e1ee1fc3a7a5c30
Shuric-git <ulme@rambler.ru>
2022-09-09 00:16:41 +0300

    fix feed layout

--- components/Newsfeed/newsfeed.styles.ts
+++ components/Newsfeed/newsfeed.styles.ts
@@ -3,13 +3,13 @@ import styled from "styled-components";
 export const Layout = styled.div`
   margin: 0;
   padding: 0;
-  width: 64vw;
   height: 100%;
   background-color: #f1f1f1;
   display: grid;
-  grid-template-columns: repeat(auto-fill, 260px);
-  grid-auto-rows: 12px;
-  gap: 10px;
+  grid-template-columns: repeat(auto-fill,23%);
+  grid-auto-rows: 10px;
+  gap: 20px;
   font-family: "Roboto";
   font-weight: 400;
+  justify-content: space-between;
 `
