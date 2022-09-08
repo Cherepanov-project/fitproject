4dc9e8cc6c85a5a8450160dc06861a75d899cf51
ninelamericanel <ninelamericanel@gmail.com>
2022-09-08 10:55:53 +0300

    deleted unnecessary style block and formatted

--- styles/admin/recipes/recipes.styles.ts
+++ styles/admin/recipes/recipes.styles.ts
@@ -5,13 +5,12 @@ export const StyledContainer = styled.div`
 `
 
 export const StyleContentList = styled.div`
-    width: 100%;
-    border: 1px solid #dfe0eb;
-    border-radius: 8px;
-    margin: 0 auto;
-    background-color: #ffffff;
+  width: 100%;
+  border: 1px solid #dfe0eb;
+  border-radius: 8px;
+  margin: 0 auto;
+  background-color: #ffffff;
 `
-export const StyleFooterRecipes = styled.div``
 
 export const StyleLoaderContainer = styled.div`
   width: 100%;
