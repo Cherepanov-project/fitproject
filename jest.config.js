3e8b8c351725f90ad1f202f28125de9254505b2b
Shuric-git <ulme@rambler.ru>
2022-09-13 16:53:28 +0300

    fix user part of jest, install cypress

--- jest.config.js
+++ jest.config.js
@@ -26,6 +26,7 @@ module.exports = {
 
         // Handle module aliases
         "^@/components/(.*)$": "<rootDir>/components/$1",
+        "^@/(.*)$": "<rootDir>$1"
     },
     // Add more setup options before each test is run
     // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
