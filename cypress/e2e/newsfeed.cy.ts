b5ce3c79b8ce2511f43597ed71cbc69790ef759e
Shuric-git <ulme@rambler.ru>
2022-09-15 22:10:06 +0300

    pull request commit (newsfeed, cypress: login, list of dishes)

--- cypress/e2e/newsfeed.cy.ts
+++ cypress/e2e/newsfeed.cy.ts
@@ -1,19 +1,4 @@
 describe('fit spec', () => {
-  // it('Gets, types and asserts', () => {
-  //   cy.visit('https://example.cypress.io')
-  //
-  //   cy.contains('type').click()
-  //
-  //   // Should be on a new URL which
-  //   // includes '/commands/actions'
-  //   cy.url().should('include', '/commands/actions')
-  //
-  //   // Get an input, type into it and verify
-  //   // that the value has been updated
-  //   cy.get('.action-email')
-  //       .type('fake@email.com')
-  //       .should('have.value', 'fake@email.com')
-  // })
   it('Gets, types and asserts', () => {
     cy.visit('http://localhost:3001/fitproject/user')
 
