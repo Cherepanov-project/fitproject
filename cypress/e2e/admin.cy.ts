eb4e5c182ae155812fd60243c853ee01981dadb8
Eugene Puzenko <puzzenko89@gmail.com>
2022-09-22 17:49:37 +0300

    it was added admin panel test

--- cypress/e2e/admin.cy.ts
+++ cypress/e2e/admin.cy.ts
@@ -12,7 +12,7 @@ describe("admin panel", () => {
 
   it("should redirect to sign up a new admin account", () => {
     cy.get('a[href="/fitproject/admin/sign-up"]').click()
-    cy.wait(3000)
+    cy.wait(5000)
   })
 
   it("all inputs should are required", () => {
@@ -38,7 +38,7 @@ describe("admin panel", () => {
 
   it("create button should create an admin account", () => {
     cy.get('button[type="submit"]').click()
-    cy.wait(3000)
+    cy.wait(5000)
   })
 
   it("redirect to login page correctly", () => {
@@ -61,6 +61,42 @@ describe("admin panel", () => {
     cy.get('button[type="submit"]').click()
   })
 
+  it("should open tabs", () => {
+    cy.get('a[href="/fitproject/admin/messages"]').click()
+    cy.wait(5000)
+    cy.get('a[href="/fitproject/admin/messages/chat/1"]').click()
+    cy.wait(3000)
+    cy.get('textarea[placeholder="Напишите сообщение ..."]').type(
+      `${generateRandomString(20)} ${generateRandomString(
+        20
+      )} ${generateRandomString(10)}`
+    )
+    cy.get('textarea[placeholder="Напишите сообщение ..."] ~ button').click()
+    cy.get('a[href="/fitproject/admin/settings"]').click()
+    cy.wait(3000)
+    cy.get('a[href="/fitproject/admin/subscription"]').click()
+    cy.wait(3000)
+    cy.get('a[href="/fitproject/admin/articles"]').click()
+    cy.wait(3000)
+    cy.get("main h2 ~ div button:last-child").click()
+    cy.get("main h2 ~ div label").click()
+    cy.get('input[name="High"]').click()
+    cy.get('input[name="Normal"]').click()
+    cy.get('input[name="Low"]').click()
+    cy.get('input[name="High"]').click()
+    cy.get('input[name="Normal"]').click()
+    cy.get('input[name="Low"]').click()
+    cy.get("main h2 ~ div div div button").click()
+    cy.get("main h2 ~ div button:nth-child(2)").click()
+    cy.wait(1000)
+    cy.get("main h2 ~ div button:first-child").click()
+    cy.get("main label:last-child div div:first-child").click()
+    cy.get("main label:last-child div div:last-child").click()
+    cy.get("main label:last-child div div:first-child").click()
+    cy.get("main label:last-child div div:last-child").click()
+    cy.get('main input[name="noSorting"]').click()
+    cy.get('ul div div a[href="/fitproject/admin/overview"]').click()
+  })
 })
 
 export {}
